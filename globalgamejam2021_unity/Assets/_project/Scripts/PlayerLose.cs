67785b94d893599a3a41aed06fad44bcb4c0e7f2
Regis Claus <regis.claus@gmail.com>
2021-01-31 18:19:27 -0300

    Nodes de Paredes e Buracos / Derrota por colisão e queda

---
+++ globalgamejam2021_unity/Assets/_project/Scripts/PlayerLose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLose : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Wall") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Wall") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
